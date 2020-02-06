a9ad36d1f2eb2d25b14b1f665fbf445fd0d20e76
Sami Eljabali <seljabali@gmail.com>
2020-02-05 20:57:09 -0700

    add images

--- design-values-system-android/app/src/main/java/com/seljabali/designvaluesystem/ui/landingpage/LandingPageItems.kt
+++ design-values-system-android/app/src/main/java/com/seljabali/designvaluesystem/ui/landingpage/LandingPageItems.kt
@@ -8,9 +8,9 @@ import com.seljabali.designvaluesystem.ui.LandingItem
 enum class LandingPageItems(@StringRes override val titleStringId: Int,
                             @DrawableRes override val iconId: Int = R.drawable.ic_launcher_background) :
     LandingItem {
-    SPACING(R.string.spacings, android.R.color.transparent),
-    TEXT_SIZES(R.string.text_sizes, android.R.color.transparent),
-    CORNER_RADIUS(R.string.corner_radiuses, android.R.color.transparent),
+    SPACING(R.string.spacings, R.mipmap.spacing),
+    TEXT_SIZES(R.string.text_sizes, R.mipmap.text_size),
+    CORNER_RADIUS(R.string.corner_radiuses, R.mipmap.corner_radius),
     ELEVATIONS(R.string.elevations, android.R.color.transparent),
-    LETTER_SPACINGS(R.string.letter_spacings, android.R.color.transparent),
+    LETTER_SPACINGS(R.string.letter_spacings, R.mipmap.letter_spacing),
 }
--- design-values-system-android/app/src/main/java/com/seljabali/designvaluesystem/ui/spacings/showcasing/SpacingsShowCasingItems.kt
+++ design-values-system-android/app/src/main/java/com/seljabali/designvaluesystem/ui/spacings/showcasing/SpacingsShowCasingItems.kt
@@ -7,7 +7,7 @@ import com.seljabali.designvaluesystem.ui.LandingItem
 
 enum class SpacingsShowCasingItems(@StringRes override val titleStringId: Int,
                                    @DrawableRes override val iconId: Int = R.drawable.ic_launcher_background) : LandingItem {
-    VERTICAL(R.string.vertical, android.R.color.transparent),
-    HORIZONTAL(R.string.horizontal, android.R.color.transparent),
-    PADDING(R.string.padding, android.R.color.transparent),
+    VERTICAL(R.string.vertical, R.mipmap.vertical_spacing),
+    HORIZONTAL(R.string.horizontal, R.mipmap.spacing),
+    PADDING(R.string.padding, R.mipmap.padding),
 }
