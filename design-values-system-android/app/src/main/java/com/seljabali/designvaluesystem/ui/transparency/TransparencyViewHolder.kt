7dc023a6500f0e16950243113a6c36e3e24ca35f
Sami Eljabali <seljabali@gmail.com>
2020-02-12 17:48:51 -0700

    show transparency values, and add screenshots

--- design-values-system-android/app/src/main/java/com/seljabali/designvaluesystem/ui/transparency/TransparencyViewHolder.kt
+++ design-values-system-android/app/src/main/java/com/seljabali/designvaluesystem/ui/transparency/TransparencyViewHolder.kt
@@ -5,14 +5,21 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.seljabali.designvaluesystem.R
 import com.seljabali.designvaluesystem.utilities.Res
+import com.seljabali.designvaluesystem.utilities.setUnderlined
 
 class TransparencyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     private val transparencyView: TextView = itemView.findViewById(R.id.transparencyView)
+    private val transparencyNameTextView: TextView = itemView.findViewById(R.id.transparencyNameTextView)
+    private val transparencyValueTextView: TextView = itemView.findViewById(R.id.transparencyValueTextView)
 
     fun bind(transparency: Transparencies) {
         val context = itemView.context
-        transparencyView.alpha = Res.getFloat(context, transparency.transparencyId)
+        val transparencyValue: Float = Res.getFloat(context, transparency.transparencyId)
+        transparencyNameTextView.text = Res.getString(context, transparency.stringId)
+        transparencyNameTextView.setUnderlined()
+        transparencyValueTextView.text = transparencyValue.toString()
+        transparencyView.alpha = transparencyValue
         itemView.invalidate()
     }
 }
