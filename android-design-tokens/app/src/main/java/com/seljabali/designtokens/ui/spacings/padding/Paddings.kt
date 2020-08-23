bed78e7302946caeb5f5b9404a9f1f979243baea
Sami Eljabali <seljabali@gmail.com>
2020-08-23 06:06:36 -0700

    remove linear spacings growth

--- android-design-tokens/app/src/main/java/com/seljabali/designtokens/ui/spacings/padding/Paddings.kt
+++ android-design-tokens/app/src/main/java/com/seljabali/designtokens/ui/spacings/padding/Paddings.kt
@@ -5,11 +5,11 @@ import androidx.annotation.StringRes
 import com.seljabali.designtokens.R
 
 enum class Paddings(@DimenRes val spacingId: Int, @StringRes val stringId: Int) {
-    XXS(R.dimen.xxs_padding_exp, R.string.xxs),
-    XS(R.dimen.xs_padding_exp, R.string.xs),
-    S(R.dimen.s_padding_exp, R.string.s),
-    M(R.dimen.m_padding_exp, R.string.m),
-    L(R.dimen.l_padding_exp, R.string.l),
-    XL(R.dimen.xl_padding_exp, R.string.xl),
-    XXL(R.dimen.xxl_padding_exp, R.string.xxl)
+    XXS(R.dimen.xxs_padding, R.string.xxs),
+    XS(R.dimen.xs_padding, R.string.xs),
+    S(R.dimen.s_padding, R.string.s),
+    M(R.dimen.m_padding, R.string.m),
+    L(R.dimen.l_padding, R.string.l),
+    XL(R.dimen.xl_padding, R.string.xl),
+    XXL(R.dimen.xxl_padding, R.string.xxl)
 }
--- android-design-tokens/app/src/main/java/com/seljabali/designtokens/ui/spacings/verticalspacings/VerticalSpacings.kt
+++ android-design-tokens/app/src/main/java/com/seljabali/designtokens/ui/spacings/verticalspacings/VerticalSpacings.kt
@@ -4,26 +4,12 @@ import androidx.annotation.DimenRes
 import androidx.annotation.StringRes
 import com.seljabali.designtokens.R
 
-// Linear
-//enum class VerticalSpacings(@DimenRes val spacingId: Int, @StringRes val stringId: Int) {
-//    XXS(R.dimen.xxs_vertical_spacing, R.string.xxs),
-//    XS(R.dimen.xs_vertical_spacing, R.string.xs),
-//    S(R.dimen.s_vertical_spacing, R.string.s),
-//    M(R.dimen.m_vertical_spacing, R.string.m),
-//    L(R.dimen.l_vertical_spacing, R.string.l),
-//    XL(R.dimen.xl_vertical_spacing, R.string.xl),
-//    XXL(R.dimen.xxl_vertical_spacing, R.string.xxl),
-//    XXXL(R.dimen.xxxl_vertical_spacing, R.string.xxxl),
-//    XL4(R.dimen.xl4_vertical_spacing, R.string.xl4)
-//}
-
-// Exponential
 enum class VerticalSpacings(@DimenRes val spacingId: Int, @StringRes val stringId: Int) {
-    XXS(R.dimen.xxs_vertical_spacing_exp, R.string.xxs),
-    XS(R.dimen.xs_vertical_spacing_exp, R.string.xs),
-    S(R.dimen.s_vertical_spacing_exp, R.string.s),
-    M(R.dimen.m_vertical_spacing_exp, R.string.m),
-    L(R.dimen.l_vertical_spacing_exp, R.string.l),
-    XL(R.dimen.xl_vertical_spacing_exp, R.string.xl),
-    XXL(R.dimen.xxl_vertical_spacing_exp, R.string.xxl)
+    XXS(R.dimen.xxs_vertical_spacing, R.string.xxs),
+    XS(R.dimen.xs_vertical_spacing, R.string.xs),
+    S(R.dimen.s_vertical_spacing, R.string.s),
+    M(R.dimen.m_vertical_spacing, R.string.m),
+    L(R.dimen.l_vertical_spacing, R.string.l),
+    XL(R.dimen.xl_vertical_spacing, R.string.xl),
+    XXL(R.dimen.xxl_vertical_spacing, R.string.xxl)
 }
